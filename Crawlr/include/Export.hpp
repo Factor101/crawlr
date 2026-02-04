c76eb4b9d06df265a688a14270974d0e4db2472b
Factor101 <61631765+Factor101@users.noreply.github.com>
2026-02-04 00:10:52 -0500

    More robust implemntation for ModuleParser - Added various gaurds and protections against invalid header values. - Removed C-Style casts. - Consolidated SyscallParser functions.

--- Crawlr/include/Export.hpp
+++ Crawlr/include/Export.hpp
@@ -6,18 +6,18 @@ namespace Crawlr
 class Export
 {
  protected:
-    const void* baseAddress;
+    void* baseAddress;
     const uint32_t rva;
     const uint32_t size;
 
  public:
     Export() = default;
-    explicit Export(const void* base) : baseAddress(base) { }
-    Export(const void* base, const uint32_t rva, const uint32_t size)
+    explicit Export(void* base) : baseAddress(base) { }
+    Export(void* base, const uint32_t rva, const uint32_t size)
         : baseAddress(base), rva(rva), size(size)
     { }
 
-    [[nodiscard]] const void* getBaseAddress() const noexcept { return baseAddress; }
+    [[nodiscard]] void* getBaseAddress() const noexcept { return baseAddress; }
     [[nodiscard]] uint32_t getSize() const noexcept { return size; }
 };
 }  // namespace Crawlr
--- Crawlr/src/SyscallParser.cpp
+++ Crawlr/src/SyscallParser.cpp
@@ -1,6 +1,5 @@
 #include "../include/SyscallParser.hpp"
 #include "../include/Signature.hpp"
-#include <type_traits>
 
 namespace Crawlr
 {
@@ -8,68 +7,41 @@ namespace SyscallParser
 {
 namespace
 {
-constexpr uint8_t SYSCALL_INSTRUCTION[] = "\x0F\x05";  // syscall
-constexpr uint8_t JMP_INSTRUCTION       = 0xE9;
+constexpr uint8_t SYSCALL_OPCODE[] = { 0x0F, 0x05 };  // syscall instruction opcode
 const Signature SYSCALL_SIGNATURE =
-    "4C 8B D1 B8 ?? ?? ?? ?? 0F 05";  // mov r10, rcx; mov eax, ?? ?? ?? ?? syscall
+    "4C 8B D1 B8 ?? ?? ?? ?? 0F 05";                  // mov r10, rcx; mov eax, ?? ?? ?? ?? syscall
+constexpr auto SYSCALL_INVOCATION_OFFSET = 8;
+constexpr auto SYSCALL_NUMBER_OFFSET     = 4;
 }  // namespace
 
-ScanResult scanExport(const Export& ex) noexcept
+[[nodiscard]] ScanResult scanExport(const Export& exp) noexcept
 {
-    ScanResult result{ false, false, nullptr };
+    ScanResult result{ false, false, nullptr, 0 };
+    uint8_t* pBase = static_cast<uint8_t*>(exp.getBaseAddress());
+    if(pBase == nullptr || exp.getSize() < SYSCALL_SIGNATURE.size())
+    {
+        return result;
+    }
+
     result.isSyscall =
-        SYSCALL_SIGNATURE.matchesAt(static_cast<uint8_t*>(ex.getBaseAddress()), ex.getSize(), 0);
+        memcmp(pBase + SYSCALL_INVOCATION_OFFSET, SYSCALL_OPCODE, sizeof(SYSCALL_OPCODE)) == 0;
 
     if(!result.isSyscall)
     {
         return result;
     }
 
-    uint8_t* pInvokeSyscall = firstSyscallInvocation(ex);
-    DWORD ssn               = findSSN(ex, pInvokeSyscall);
-}
+    result.matchesUnhookedSyscall = SYSCALL_SIGNATURE.matches(pBase, exp.getSize());
 
-// able to pass in void* or Export-derived class
-template<typename T>
-    requires std::is_base_of_v<Crawlr::Export, T>
-uint8_t* firstSyscallInvocation(const T& ex) noexcept
-{
-    uint8_t* pBase = static_cast<uint8_t*>(ex.getBaseAddress());
-    size_t size    = ex.getSize();
-    size_t offset  = SYSCALL_SIGNATURE.matchFirst(pBase, size);  // find first syscall invocation
-
-    if(offset != Signature::npos)
+    if(!result.matchesUnhookedSyscall)
     {
-        return pBase + offset;
+        return result;
     }
 
-    return nullptr;
+    result.pSyscallOpcode = pBase + SYSCALL_INVOCATION_OFFSET;
+    result.syscallNumber  = *reinterpret_cast<DWORD*>(pBase + SYSCALL_NUMBER_OFFSET);
+
+    return result;
 }
-
-DWORD findSSN(const Export& ex, uint8_t* pInvokeSyscall)
-{
-    uint8_t* pBase = static_cast<uint8_t*>(ex.getBaseAddress());
-    size_t size    = ex.getSize();
-    if(pInvokeSyscall == nullptr)
-    {
-        return 0;
-    }
-    // parse for EAX value
-    DWORD ssn = 0;
-    for(auto i = 0; i < 0x20; i++)
-    {
-        PBYTE pCurrentByte = (PBYTE)pFunctionBase + i;
-        if(memcmp(pCurrentByte, syscallSignature, sizeof(syscallSignature)) == 0)
-        {
-            ssn = *(PBYTE)(pCurrentByte + sizeof(syscallSignature));
-            break;
-        }
-    }
-
-
-    return ssn;
-}
-
 }  // namespace SyscallParser
 }  // namespace Crawlr
-
